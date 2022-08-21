ff0e78556e0621ee1d6c9d75a36814a979842f66
Tarrow <visionaryvj@gmail.com>
2022-08-22 00:18:23 +0200

    fixed the a bug so the app clone can be run

--- Workspace/App.js
+++ Workspace/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import AppStack from './src/routes/AppStack';
 
-const Stack = createNativeStackNavigator();
 
 const App = () => {
   return(
-    <AppStack/>
+    <AppStack/> 
   )
 }
 
